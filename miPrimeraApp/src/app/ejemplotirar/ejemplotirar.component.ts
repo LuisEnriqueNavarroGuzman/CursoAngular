0aa4d42cf2f943f1fd9c5609eb500bd5b2f479f4
LuisEnriqueNavarroGuzman <navarroguzman1999@gmail.com>
2023-03-10 19:41:30 -0600

    video 6

---
+++ miPrimeraApp/src/app/ejemplotirar/ejemplotirar.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ejemplotirar',
+  // utilizar ` ` si el codigo inline esta distribuido en varias lineas
+  template: `
+    <p>
+      ejemplotirar works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class EjemplotirarComponent {
+
+}
--- miPrimeraApp/src/app/empleado/empleado.component.ts
+++ miPrimeraApp/src/app/empleado/empleado.component.ts
@@ -4,6 +4,9 @@ import { Component } from '@angular/core';
   selector: 'app-empleado',
   templateUrl: './empleado.component.html',
   styleUrls: ['./empleado.component.css']
+  //Componente inline no necesita archivo html ni css
+  //template: "<p>Aquí iría un empleado</p>",
+  //styles : ["p{background-color: red;}"]
 })
 export class EmpleadoComponent {
 
